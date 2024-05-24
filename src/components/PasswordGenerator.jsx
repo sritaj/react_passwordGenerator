9b91dab7b232613679c09a40f9a68ff2aca4c3ca
Sritaj Patel <sritajp@gmail.com>
2024-05-25 03:40:41 +0530

    Added react font awesome packages, and added two icons on either end of the input box

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy, faLock } from "@fortawesome/free-solid-svg-icons";
+
 const PasswordGenerator = () => {
   return (
     <div className="w-full h-screen bg-gradient-to-r from-cyan-50 to-cyan-100 flex justify-center">
@@ -8,11 +11,17 @@ const PasswordGenerator = () => {
         <p className="text-xs text-center py-4 font-light font-mono">
           Generate a secure and random password
         </p>
-        <div className="">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center cursor-pointer">
+            <FontAwesomeIcon icon={faLock} style={{ color: "#38997c" }} />
+          </div>
           <input
             className="text-sm rounded-2xl border border-gray-300 text-gray-900 h-8 w-full align-middle text-center ps-8 pe-8"
             readOnly={false}
           ></input>
+          <div className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
+            <FontAwesomeIcon icon={faCopy} style={{ color: "#dbdbdb" }} />
+          </div>
         </div>
       </div>
     </div>
