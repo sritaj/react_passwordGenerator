2b6e8424fe8aee1170440d0675d2edea78159353
Sritaj Patel <sritajp@gmail.com>
2024-05-25 14:25:16 +0530

    Added range slider

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -34,6 +34,21 @@ const PasswordGenerator = () => {
             <SettingsMenu id="numbers" checkboxName="Numbers" />
           </div>
         </div>
+        <div className="pt-4">
+          <label htmlFor="range" className="text-xs font-bold font-serif">
+            Length
+          </label>
+          <input
+            id="range"
+            type="range"
+            minLength={8}
+            maxLength={64}
+            step={1}
+            value={8}
+            onChange={() => console.log("changing")}
+            className="w-full rounded-lg cursor-pointer bg-gray-100"
+          ></input>
+        </div>
       </div>
     </div>
   );
